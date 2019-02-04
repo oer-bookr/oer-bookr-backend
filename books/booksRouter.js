66d687a2807bdc49480d0166e7b9bb840ac18e7c
Joseph <josephryan96@gmail.com>
2019-02-03 23:49:42 -0800

    Testing endpoints on heroku

--- books/booksRouter.js
+++ books/booksRouter.js
@@ -2,4 +2,9 @@ const express = require("express");
 
 const router = express.Router();
 
+router.get("/books", (req, res) => {
+  res.status(200).json("working!");
+  console.log("working");
+});
+
 module.exports = router;
--- reviews/reviewsRouter.js
+++ reviews/reviewsRouter.js
@@ -2,4 +2,9 @@ const express = require("express");
 
 const router = express.Router();
 
+router.get("/review", (req, res) => {
+  res.status(200).json("working!");
+  console.log("working");
+});
+
 module.exports = router;
