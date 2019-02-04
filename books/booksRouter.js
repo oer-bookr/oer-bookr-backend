741bd42b8ba2c7a862f6eee1a47a4a6e2f6934aa
Joseph <josephryan96@gmail.com>
2019-02-04 00:04:42 -0800

    fixed errors

--- books/booksRouter.js
+++ books/booksRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/books", (req, res) => {
+router.get("/", (req, res) => {
   res.status(200).json("working!");
   console.log("working");
 });
