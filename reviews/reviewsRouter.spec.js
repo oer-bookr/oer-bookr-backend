2f8a5da2953e7b9b41c4d9f3c0ea82ee873d6e73
Joseph <josephryan96@gmail.com>
2019-02-07 09:39:14 -0800

    Added reviews tests

--- reviews/reviewsRouter.spec.js
+++ reviews/reviewsRouter.spec.js
@@ -5,41 +5,38 @@ const db = require("../data/dbConfig.js");
 const server = require("../api/server.js");
 
 afterEach(async () => {
-  await db("books").truncate();
+  await db("reviews").truncate();
 });
 
 describe("server.js", async () => {
-  describe("GET /books endpoint", async () => {
+  describe("GET /reviews endpoint", async () => {
     it("should respond with status code 200", async () => {
-      const response = await request(server).get("/books");
+      const response = await request(server).get("/reviews");
       expect(response.status).toBe(200);
     });
 
     it("should respond with JSON object", async () => {
-      const response = await request(server).get("/books");
+      const response = await request(server).get("/reviews");
       expect(response.type).toBe("application/json");
     });
 
     it("should respond with an array", async () => {
-      const response = await request(server).get("/books");
+      const response = await request(server).get("/reviews");
       expect(Array.isArray(response.body)).toBe(true);
     });
   });
 
-  describe("POST /books endpoint", () => {
+  describe("POST /reviews endpoint", () => {
     it("should post to server & respond with 201 status code", async () => {
       const body = {
-        title: "Damnation (Karhozat)",
-        author: "Maggy Earle",
-        publisher: "Ingunna Bartram",
-        license: "S0421XA",
-        subject: "Drama|Film-Noir",
-        image: "http://dummyimage.com/121x198.bmp/ff4444/ffffff",
-        link: "http://yahoo.co.jp/venenatis/tristique/fusce/congue.json"
+        book_id: 1,
+        reviewer: "Test Reviewer",
+        review: "Test Review",
+        rating: 5
       };
 
       let response = await request(server)
-        .post("/books")
+        .post("/reviews")
         .send(body);
       expect(response.body).toEqual([1]);
       expect(response.status).toBe(201);
@@ -47,15 +44,12 @@ describe("server.js", async () => {
 
     it("should fail & respond with 422 status code", async () => {
       const body = {
-        title: "Damnation (Karhozat)",
-        author: "Maggy Earle",
-        publisher: "Ingunna Bartram",
-        license: "S0421XA",
-        subject: "Drama|Film-Noir"
+        book_id: 1,
+        reviewer: "Test Reviewer"
       };
 
       let response = await request(server)
-        .post("/books")
+        .post("/reviews")
         .send(body);
 
       expect(response.status).toBe(422);
@@ -63,33 +57,30 @@ describe("server.js", async () => {
 
     it("should respond with JSON object", async () => {
       let response = await request(server)
-        .post("/books")
+        .post("/reviews")
         .send({
-          title: "Damnation (Karhozat)",
-          author: "Maggy Earle",
-          publisher: "Ingunna Bartram",
-          license: "S0421XA",
-          subject: "Drama|Film-Noir",
-          image: "http://dummyimage.com/121x198.bmp/ff4444/ffffff",
-          link: "http://yahoo.co.jp/venenatis/tristique/fusce/congue.json"
+          book_id: 1,
+          reviewer: "Test Reviewer",
+          review: "Test Review",
+          rating: 5
         });
       expect(response.type).toBe("application/json");
     });
   });
 
-  describe("DELETE /books/:id endpoint", async () => {
+  describe("DELETE /reviews/:id endpoint", async () => {
     it("should respond with status code 200", async () => {
-      const response = await request(server).get("/books/:id");
+      const response = await request(server).get("/reviews/:id");
       expect(response.status).toBe(200);
     });
 
     it("should respond with JSON object", async () => {
-      const response = await request(server).get("/books/:id");
+      const response = await request(server).get("/reviews/:id");
       expect(response.type).toBe("application/json");
     });
 
     it("should respond with count", async () => {
-      const response = await request(server).get("/books/:id");
+      const response = await request(server).get("/reviews/:id");
       expect(response.body).toEqual(1);
     });
   });
