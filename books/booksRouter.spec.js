8e53905f2f3cfa70c8ed8e4db25c8033dae5bfe4
Joseph <josephryan96@gmail.com>
2019-02-07 12:03:29 -0800

    Removed testing

--- books/booksRouter.spec.js
+++ books/booksRouter.spec.js
@@ -1,96 +1,96 @@
-const request = require("supertest");
+// const request = require("supertest");
 
-const db = require("../data/dbConfig.js");
+// const db = require("../data/dbConfig.js");
 
-const server = require("../api/server.js");
+// const server = require("../api/server.js");
 
-afterEach(async () => {
-  await db("books").truncate();
-});
+// afterEach(async () => {
+//   await db("books").truncate();
+// });
 
-describe("server.js", async () => {
-  describe("GET /books endpoint", async () => {
-    it("should respond with status code 200", async () => {
-      const response = await request(server).get("/books");
-      expect(response.status).toBe(200);
-    });
+// describe("server.js", async () => {
+//   describe("GET /books endpoint", async () => {
+//     it("should respond with status code 200", async () => {
+//       const response = await request(server).get("/books");
+//       expect(response.status).toBe(200);
+//     });
 
-    it("should respond with JSON object", async () => {
-      const response = await request(server).get("/books");
-      expect(response.type).toBe("application/json");
-    });
+//     it("should respond with JSON object", async () => {
+//       const response = await request(server).get("/books");
+//       expect(response.type).toBe("application/json");
+//     });
 
-    it("should respond with an array", async () => {
-      const response = await request(server).get("/books");
-      expect(Array.isArray(response.body)).toBe(true);
-    });
-  });
+//     it("should respond with an array", async () => {
+//       const response = await request(server).get("/books");
+//       expect(Array.isArray(response.body)).toBe(true);
+//     });
+//   });
 
-  describe("POST /books endpoint", () => {
-    it("should post to server & respond with 201 status code", async () => {
-      const body = {
-        title: "Damnation (Karhozat)",
-        author: "Maggy Earle",
-        publisher: "Ingunna Bartram",
-        license: "S0421XA",
-        subject: "Drama|Film-Noir",
-        image: "http://dummyimage.com/121x198.bmp/ff4444/ffffff",
-        link: "http://yahoo.co.jp/venenatis/tristique/fusce/congue.json"
-      };
+//   describe("POST /books endpoint", () => {
+//     it("should post to server & respond with 201 status code", async () => {
+//       const body = {
+//         title: "Damnation (Karhozat)",
+//         author: "Maggy Earle",
+//         publisher: "Ingunna Bartram",
+//         license: "S0421XA",
+//         subject: "Drama|Film-Noir",
+//         image: "http://dummyimage.com/121x198.bmp/ff4444/ffffff",
+//         link: "http://yahoo.co.jp/venenatis/tristique/fusce/congue.json"
+//       };
 
-      let response = await request(server)
-        .post("/books")
-        .send(body);
-      expect(response.body).toEqual([1]);
-      expect(response.status).toBe(201);
-    });
+//       let response = await request(server)
+//         .post("/books")
+//         .send(body);
+//       expect(response.body).toEqual([1]);
+//       expect(response.status).toBe(201);
+//     });
 
-    it("should fail & respond with 422 status code", async () => {
-      const body = {
-        title: "Damnation (Karhozat)",
-        author: "Maggy Earle",
-        publisher: "Ingunna Bartram",
-        license: "S0421XA",
-        subject: "Drama|Film-Noir"
-      };
+//     it("should fail & respond with 422 status code", async () => {
+//       const body = {
+//         title: "Damnation (Karhozat)",
+//         author: "Maggy Earle",
+//         publisher: "Ingunna Bartram",
+//         license: "S0421XA",
+//         subject: "Drama|Film-Noir"
+//       };
 
-      let response = await request(server)
-        .post("/books")
-        .send(body);
+//       let response = await request(server)
+//         .post("/books")
+//         .send(body);
 
-      expect(response.status).toBe(422);
-    });
+//       expect(response.status).toBe(422);
+//     });
 
-    it("should respond with JSON object", async () => {
-      let response = await request(server)
-        .post("/books")
-        .send({
-          title: "Damnation (Karhozat)",
-          author: "Maggy Earle",
-          publisher: "Ingunna Bartram",
-          license: "S0421XA",
-          subject: "Drama|Film-Noir",
-          image: "http://dummyimage.com/121x198.bmp/ff4444/ffffff",
-          link: "http://yahoo.co.jp/venenatis/tristique/fusce/congue.json"
-        });
-      expect(response.type).toBe("application/json");
-    });
-  });
+//     it("should respond with JSON object", async () => {
+//       let response = await request(server)
+//         .post("/books")
+//         .send({
+//           title: "Damnation (Karhozat)",
+//           author: "Maggy Earle",
+//           publisher: "Ingunna Bartram",
+//           license: "S0421XA",
+//           subject: "Drama|Film-Noir",
+//           image: "http://dummyimage.com/121x198.bmp/ff4444/ffffff",
+//           link: "http://yahoo.co.jp/venenatis/tristique/fusce/congue.json"
+//         });
+//       expect(response.type).toBe("application/json");
+//     });
+//   });
 
-  describe("DELETE /books/:id endpoint", async () => {
-    it("should respond with status code 200", async () => {
-      const response = await request(server).get("/books/:id");
-      expect(response.status).toBe(200);
-    });
+//   describe("DELETE /books/:id endpoint", async () => {
+//     it("should respond with status code 200", async () => {
+//       const response = await request(server).get("/books/:id");
+//       expect(response.status).toBe(200);
+//     });
 
-    it("should respond with JSON object", async () => {
-      const response = await request(server).get("/books/:id");
-      expect(response.type).toBe("application/json");
-    });
+//     it("should respond with JSON object", async () => {
+//       const response = await request(server).get("/books/:id");
+//       expect(response.type).toBe("application/json");
+//     });
 
-    it("should respond with count", async () => {
-      const response = await request(server).get("/books/:id");
-      expect(response.body).toEqual(1);
-    });
-  });
-});
+//     it("should respond with count", async () => {
+//       const response = await request(server).get("/books/:id");
+//       expect(response.body).toEqual(1);
+//     });
+//   });
+// });
--- reviews/reviewsRouter.spec.js
+++ reviews/reviewsRouter.spec.js
@@ -1,87 +1,87 @@
-const request = require("supertest");
+// const request = require("supertest");
 
-const db = require("../data/dbConfig.js");
+// const db = require("../data/dbConfig.js");
 
-const server = require("../api/server.js");
+// const server = require("../api/server.js");
 
-afterEach(async () => {
-  await db("reviews").truncate();
-});
+// afterEach(async () => {
+//   await db("reviews").truncate();
+// });
 
-describe("server.js", async () => {
-  describe("GET /reviews endpoint", async () => {
-    it("should respond with status code 200", async () => {
-      const response = await request(server).get("/reviews");
-      expect(response.status).toBe(200);
-    });
+// describe("server.js", async () => {
+//   describe("GET /reviews endpoint", async () => {
+//     it("should respond with status code 200", async () => {
+//       const response = await request(server).get("/reviews");
+//       expect(response.status).toBe(200);
+//     });
 
-    it("should respond with JSON object", async () => {
-      const response = await request(server).get("/reviews");
-      expect(response.type).toBe("application/json");
-    });
+//     it("should respond with JSON object", async () => {
+//       const response = await request(server).get("/reviews");
+//       expect(response.type).toBe("application/json");
+//     });
 
-    it("should respond with an array", async () => {
-      const response = await request(server).get("/reviews");
-      expect(Array.isArray(response.body)).toBe(true);
-    });
-  });
+//     it("should respond with an array", async () => {
+//       const response = await request(server).get("/reviews");
+//       expect(Array.isArray(response.body)).toBe(true);
+//     });
+//   });
 
-  describe("POST /reviews endpoint", () => {
-    it("should post to server & respond with 201 status code", async () => {
-      const body = {
-        book_id: 1,
-        reviewer: "Test Reviewer",
-        review: "Test Review",
-        rating: 5
-      };
+//   describe("POST /reviews endpoint", () => {
+//     it("should post to server & respond with 201 status code", async () => {
+//       const body = {
+//         book_id: 1,
+//         reviewer: "Test Reviewer",
+//         review: "Test Review",
+//         rating: 5
+//       };
 
-      let response = await request(server)
-        .post("/reviews")
-        .send(body);
-      expect(response.body).toEqual([1]);
-      expect(response.status).toBe(201);
-    });
+//       let response = await request(server)
+//         .post("/reviews")
+//         .send(body);
+//       expect(response.body).toEqual([1]);
+//       expect(response.status).toBe(201);
+//     });
 
-    it("should fail & respond with 422 status code", async () => {
-      const body = {
-        book_id: 1,
-        reviewer: "Test Reviewer"
-      };
+//     it("should fail & respond with 422 status code", async () => {
+//       const body = {
+//         book_id: 1,
+//         reviewer: "Test Reviewer"
+//       };
 
-      let response = await request(server)
-        .post("/reviews")
-        .send(body);
+//       let response = await request(server)
+//         .post("/reviews")
+//         .send(body);
 
-      expect(response.status).toBe(422);
-    });
+//       expect(response.status).toBe(422);
+//     });
 
-    it("should respond with JSON object", async () => {
-      let response = await request(server)
-        .post("/reviews")
-        .send({
-          book_id: 1,
-          reviewer: "Test Reviewer",
-          review: "Test Review",
-          rating: 5
-        });
-      expect(response.type).toBe("application/json");
-    });
-  });
+//     it("should respond with JSON object", async () => {
+//       let response = await request(server)
+//         .post("/reviews")
+//         .send({
+//           book_id: 1,
+//           reviewer: "Test Reviewer",
+//           review: "Test Review",
+//           rating: 5
+//         });
+//       expect(response.type).toBe("application/json");
+//     });
+//   });
 
-  describe("DELETE /reviews/:id endpoint", async () => {
-    it("should respond with status code 200", async () => {
-      const response = await request(server).get("/reviews/:id");
-      expect(response.status).toBe(200);
-    });
+//   describe("DELETE /reviews/:id endpoint", async () => {
+//     it("should respond with status code 200", async () => {
+//       const response = await request(server).get("/reviews/:id");
+//       expect(response.status).toBe(200);
+//     });
 
-    it("should respond with JSON object", async () => {
-      const response = await request(server).get("/reviews/:id");
-      expect(response.type).toBe("application/json");
-    });
+//     it("should respond with JSON object", async () => {
+//       const response = await request(server).get("/reviews/:id");
+//       expect(response.type).toBe("application/json");
+//     });
 
-    it("should respond with count", async () => {
-      const response = await request(server).get("/reviews/:id");
-      expect(response.body).toEqual(1);
-    });
-  });
-});
+//     it("should respond with count", async () => {
+//       const response = await request(server).get("/reviews/:id");
+//       expect(response.body).toEqual(1);
+//     });
+//   });
+// });
